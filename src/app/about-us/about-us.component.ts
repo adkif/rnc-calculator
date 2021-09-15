e04bee52bc1df369e8a06fd2a6592b319a76889e
adkif <akifungo@gmail.com>
2021-09-15 18:53:04 +0200

    update about us

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent implements OnInit {
-
+  team = [
+    {name: "Adolphe Kifungo", sprite: "male"},
+    {name: "Vianney Hekima", sprite: "male"},
+    {name: "Joëlle Zawadi", sprite: "female"},
+    {name: "Abija Musavuli", sprite: "male"},
+    {name: "David Tsongo", sprite: "male"}
+  ]
+  director = {
+    name:"Ph.D Olivier Mushage",
+    sprite:"male"
+  }
   constructor() { }
 
   ngOnInit(): void {
